a413afc5eaf3966d8bfea77d498c964d28bde902
Yeom-A-Hyeon <20180982@dongduk.ac.kr>
2024-03-29 19:25:54 +0900

    create ahyeonVC

---
+++ chilin'CheerUp/chilin'CheerUp/ahyeonVC.swift
@@ -0,0 +1,29 @@
+//
+//  ahyeonVC.swift
+//  chilin'CheerUp
+//
+//  Created by A Hyeon on 3/29/24.
+//
+
+import UIKit
+
+class ahyeonVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
