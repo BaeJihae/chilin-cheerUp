671d5f669689b8b6f19a69d3c4d7b362b153393e
ljining <108book@naver.com>
2024-03-29 19:31:45 +0900

    1

---
+++ chilin'CheerUp/chilin'CheerUp/NewViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NewViewController.swift
+//  chilin'CheerUp
+//
+//  Created by 이유진 on 3/29/24.
+//
+
+import UIKit
+
+class NewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
