6a67be96455082e2c21d5ec16d3fdcc384929f15
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-03-29 19:26:55 +0900

    jihaeFileUpload

---
+++ chilin'CheerUp/chilin'CheerUp/JihaeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  JihaeViewController.swift
+//  chilin'CheerUp
+//
+//  Created by 배지해 on 3/29/24.
+//
+
+import UIKit
+
+class JihaeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
